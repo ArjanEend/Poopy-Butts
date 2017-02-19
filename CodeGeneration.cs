5bd0874eeb2a4b854deac93ac7dc19fc0ac34579
Arjan Cordia <arjaneend@gmail.com>
2017-02-19 16:30:08 +0100

    ADD start for code generation

---
+++ CodeGeneration.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TexTastic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < 10; i++)
+            {
+                FileStream fStream = new FileStream("../../Assets/Source/Generated/" + i + ".cs", FileMode.Create);
+                StreamWriter writer = new StreamWriter(fStream);
+                writer.Write(new ClassBuilder().StringBuilder.ToString());
+
+                writer.Flush();
+                writer.Dispose();
+                fStream.Dispose();
+            }
+        }
+    }
+}
