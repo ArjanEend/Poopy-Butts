8b1929d980110c07031219427c0f9770e888a533
Arjan_School <arjaneend@gmail.com>
2017-04-06 16:30:17 +0200

    FIX main thread problem in PingView

--- Assets/Scripts/Controllers/PingView.cs
+++ Assets/Scripts/Controllers/PingView.cs
@@ -7,12 +7,19 @@ public class PingView : MonoBehaviour
     [SerializeField]
     private Text pingDisplay;
 
+    private long ping;
+
     public void Initialize(PingSystem system)
     {
         system.OnPingUpdated += OnPingUpdate;
     }
 
     private void OnPingUpdate(long ping)
+    {
+        this.ping = ping;
+    }
+
+    private void Update()
     {
         pingDisplay.text = ping.ToString();
     }
