bd0188ef4849b2649f3fd7683ab04dca7b24fd52
Arjan_School <arjaneend@gmail.com>
2017-04-26 14:13:32 +0200

    ADD entityReference

---
+++ Assets/Source/Implementation/Components/Tilemap.cs
@@ -0,0 +1,13 @@
+﻿using RocketWorks.Entities;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PoopyButts.Components
+{
+    public class Tilemap : IComponent
+    {
+        private int[,] tiles;
+        private int assetId;
+    }
+}
