6cad7f75a7ea79178cd93af16af4d3a32c1825f1
Arjan <arjan@rockitbit.com>
2017-05-31 11:28:48 +0200

    ADD postprocessing & cool rotation

---
+++ Assets/Scripts/CoolRotation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoolRotation : MonoBehaviour {
+
+    private Vector3 prevPos;
+    private Vector3 eulerAngles = Vector3.zero;
+
+	void Start () {
+        prevPos = transform.position;
+	}
+	
+	void Update () {
+        Vector3 diff = transform.position - prevPos;
+        prevPos = transform.position;
+
+        diff = transform.InverseTransformDirection(diff);
+
+        eulerAngles.x = Mathf.Lerp(eulerAngles.x, diff.z * 90f, Time.deltaTime * 6f);
+        eulerAngles.z = Mathf.Lerp(eulerAngles.z, diff.x * 90f, Time.deltaTime * 6f);
+
+        transform.localEulerAngles = eulerAngles;
+	}
+}
